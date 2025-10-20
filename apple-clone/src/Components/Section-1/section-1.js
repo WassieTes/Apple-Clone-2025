6e1af5077939f8151a7306808773e0ad8f52f85f
Wassie Tesfaye <nafal0016@gmail.com>
2025-10-21 02:11:40 +0300

    Updated Middle Sections

---
+++ apple-clone/src/Components/Section-1/section-1.js
@@ -0,0 +1,11 @@
+
+     <!-- section-one -->
+      <div class="iphone17-pro">
+      <h1>iphone 17 Pro</h1>
+      <h7>All Out Pro</h7>
+      <div class="button">
+      <button class="but1"><a>Learn more</a></button>
+      <button class="but2"><a>Buy</a></button>
+      </div>
+      </div><br>
+     <!-- section-one -->
