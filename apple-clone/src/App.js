208df96d617cf15be2e4b5b6ac311b9e5369a2e0
Wassie Tesfaye <nafal0016@gmail.com>
2025-10-21 17:19:11 +0300

    Section-4 imported

--- apple-clone/src/App.js
+++ apple-clone/src/App.js
@@ -3,12 +3,16 @@ import Header from "./Components/Header/Header.js"
 import Footer from "./Components/Footer/Footer.js";
 import Section1 from "./Components/Section-1/section1.js"
 import Section2 from "./Components/Section-2/section2.js"
+import Section3 from "./Components/Section-3/section3.js"
+import Section4 from "./Components/Section-4/section4.js"
 function App() {
   return (
     <>
       <Header />
       <Section1 />
-      < Section2 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
       <Footer />
     </>
   );
---
+++ apple-clone/src/Components/Section-3/section3.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./section3.css";
+export default function section3() {
+  return (
+    <>
+       <div className="iphone-17">
+        <h1>iphone 17 </h1>
+        <p>Magichromatic</p>
+         <div className="button3">
+          <button className="but5"><a>Learn more</a></button>
+          <button className="but6"><a>Buy</a></button>
+      </div>
+      </div><br/>
+    </>
+  )
+}
