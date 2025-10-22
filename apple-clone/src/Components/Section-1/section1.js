7e3c23e469c10d9debb196e663947e64b6dbd2c2
Wassie Tesfaye <nafal0016@gmail.com>
2025-10-22 03:42:26 +0300

    Updated Section 1 imported

--- apple-clone/src/Components/Section-1/section1.js
+++ apple-clone/src/Components/Section-1/section1.js
@@ -1,26 +1,17 @@
 import React from 'react'
-import "./section1.css";
+import "./section1.css"
 
-export default function Section1() {
+export default function section1() {
   return (
     <>
-      <div className="iphone17-pro">
-        <h1>iphone 17 Pro</h1>
-        <h7>All Out Pro</h7>
-        <div className="button">
-          <button className="but1">
-            <a href="#">Learn more</a>
-          </button>
-          <button className="but2">
-            <a href = "#"  >Buy</a>
-          </button>
-        </div>
+       <div class="iphone17-pro">
+      <h1>iphone 17 Pro</h1>
+      <h7>All Out Pro</h7>
+      <div class="button">
+      <button class="but1"><a>Learn more</a></button>
+      <button class="but2"><a>Buy</a></button>
       </div>
-      <br/>
+      </div><br/>
     </>
-  );
+  )
 }
-
-
-   
-     
