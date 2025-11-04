d56b942fda2f68e324b51bd7a4ddfaac40f96dbb
Wassie Tesfaye <nafal0016@gmail.com>
2025-11-04 23:47:32 +0300

    Signup Table Created in my Database

--- apple-clone/BackEnd/server.js
+++ apple-clone/BackEnd/server.js
@@ -1,24 +1,36 @@
-const mysql = require("mysql2");
-const express = require("express");
+const express = require("express")
+const mysql = require("mysql2")
+
 
 var app = express();
 
-app.listen(3001 , () => {
-    console.log("listening")
+app.listen(3004, () => {
+    console.log("Listening ......")
 });
 
-var db = mysql.createConnection({
+const DB = mysql.createConnection({
     user: "apple",
+    host: "localhost",
     password: "apple",
-    host: "127.0.0.1",
-    database: "apple",
+    database: "apple"
 });
 
-db.connect((err) => {
-    if(err){
-        console.log(err);
-    }else{
-        console.log("connected");
-    }
+DB.connect((err) => {
+    if(err) console.log(err);
+    else console.log("connected to my Datbase .....")
+});
 
+app.get ("/signup" , (req,res) => {
+    let massage = "Table created";
+    let Signup1 = `CREATE TABLE IF NOT EXISTS signup(
+         id INT AUTO_INCREMENT PRIMARY KEY,
+         firstname VARCHAR(255) NOT NULL,
+         email VARCHAR(255) NOT NULL,
+         password VARCHAR(255) NOT NULL
+    )`;
+  DB.query(Signup1, (err,results,fields) => {
+    if(err) console.log(err);
+  });
+
+    res.end(massage);
 });
